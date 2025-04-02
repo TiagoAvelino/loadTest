229ef5c7158683f3eb59c9a1d09bfc43bbac7110
Tiago Avelino <tiago.avelino1997@gmail.com>
2025-04-02 10:34:26 -0300

    files moved to the wrong directory

--- k6/load-test.js
+++ k6/load-test.js
@@ -23,29 +23,9 @@ var i = 0;
         });
         const randomNum = Math.floor(Math.random() * 50001);
         const headers = { 'Content-Type': 'application/json','Accept':'*/*' };
-        const url = `http://mqtt-producer-kafka.apps.tiago-cluster.sandbox2300.opentlc.com/mqtt/send?topic=mqtt-message-in/${randomNum}/2/app/test`;
+        const url = `http://mqtt-producer-kafka.apps.tiago-cluster.sandbox1900.opentlc.com/mqtt/send?topic=mqtt-message-in/${randomNum}/2/app/test`;
         const res = http.post(url, payload, { headers });
-
         check(res, {
             'Post status is 200': (r) => res.status === 200
         });
     }
-
-//curl -X POST \
-// http://localhost:8080/mqtt/send?topic=mqtt-message-in/1/2/app/test \
-// -H "Content-Type: application/json" \
-// -H "Accept: */*" \
-// -d '{
-//       "message": "teste1",
-//       "jwt": "teste"
-//     }'
-
-
-// curl -X POST "http://mqtt-producer-horus.apps.tiago-cluster.sandbox3073.opentlc.com/mqtt/send?topic=mqtt-message-in/1/2/app/test" \
-// -H "Content-Type: application/json" \
-// -H "Accept: */*" \
-// -d '{"message": "teste1", "jwt": "teste"}'
-// curl -X POST "http://mqtt-producer-kafka.apps.tiago-cluster.sandbox2300.opentlc.com/mqtt/send?topic=mqtt-message-in/1/2/app/test" \
-// -H "Content-Type: application/json" \
-// -H "Accept: */*" \
-// -d '{"message": "teste1", "jwt": "teste"}'
